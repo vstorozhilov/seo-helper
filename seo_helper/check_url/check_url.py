7b7f9376850760d8569b2ac6eaf7df0c238ce08f
vstorozhilov <vl.storozhilov@gmail.com>
2023-09-30 23:54:48 +0300

    change API endpoint to POST

--- seo_helper/check_url/check_url.py
+++ seo_helper/check_url/check_url.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from ..functions import *
 from ..topics_loader import topics
+from pydantic import BaseModel
+
+class Url(BaseModel):
+    url: str
 
 
 router = APIRouter(
@@ -8,12 +12,10 @@ router = APIRouter(
 )
 
 
-@router.get("/check_url")
-async def check_url(url : str):
-    print(topics)
-    print(url)
+@router.post("/check_url")
+async def check_url(url : Url):
+    url = url.model_dump()['url']
     url = url.replace("&", "")
-    print('tuta')
     json_response = await get_search_results(url)
     gpt_query_text = compile_chatgpt_query(url, json_response)
     topic = await get_chatgpt_answer(gpt_query_text)
--- seo_helper/functions.py
+++ seo_helper/functions.py
@@ -14,10 +14,8 @@ STRING_TOPIC_LIST = ', '.join(topics.keys())
 
 
 async def get_search_results(url):
-    print('get search result start')
     async with AsyncClient() as ac:
         response = await ac.get(URL_TO_SEARCH_API.format(CUSTOM_SEARCH_API_KEY, CUSTOM_ENGINE_ID, url))
-    print(response.status_code)
     return response.json() if response.status_code == 200 else {}
 
 
@@ -29,16 +27,12 @@ def compile_chatgpt_query(url, json_response):
         gpt_query_text += item['snippet']
     gpt_query_text += "\""
 
-    print(gpt_query_text)
-
     return gpt_query_text
 
 
 async def get_chatgpt_answer(gpt_query_text :  str):
     
     message_to_gpt = MESSAGE_TO_GPT_PATTERN.format(gpt_query_text)
-
-    print(STRING_TOPIC_LIST)
 
     history_context = {
         "role": "assistant",
